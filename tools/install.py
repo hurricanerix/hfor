4dc80af2453a216aaf95d177816d6bd26aabfd8e
Richard Hawkins <hurricanerix@gmail.com>
2017-03-02 22:58:19 -0600

    Update hfor

---
+++ tools/install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+
+def main():
+    print('TODO: write install script')
+    # Install ZSH
+    # TODO: sudo chsh -s `which zsh` $USER
+    # TODO: sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    # TODO: link dotfiles
+
+
+if __name__ == '__main__':
+    main()
